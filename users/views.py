264501d7b568077b82ebe1ba2ff987592915f35d
JanVanAik <ar.orlovskiy@gmail.com>
2024-01-26 22:56:00 +0300

    reworked users template, added UserLoginForm

--- users/views.py
+++ users/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponseRedirect
+from users.forms import UserLoginForm
+from django.urls import reverse
+from django.contrib import auth
 # Create your views here.
 
 
 def users(request):
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse(""))
+    else:
+        form = UserLoginForm()
     context = {
-        "title": "Пользователь"
+        "title": "Авторизация",
+        'form': form
     }
     return render(request, 'users/login.html', context)
 
