3dd8a57eb421c9e2c595c6dfc83b29500fc673f6
JanVanAik <ar.orlovskiy@gmail.com>
2024-01-27 14:32:14 +0300

    registration form

--- users/forms.py
+++ users/forms.py
@@ -1,24 +1,33 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django import forms
 from users.models import User
 
 
 class UserLoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput
-    (attrs={'class': 'form-control py-4', "placeholder": "Введите имя пользователя"}))
+        (attrs={'class': 'form-control py-4', "placeholder": "Введите имя пользователя"}))
     password = forms.CharField(widget=forms.PasswordInput
-    (attrs={'class': 'form-control py-4', "placeholder": "Введите  пароль"}))
+        (attrs={'class': 'form-control py-4', "placeholder": "Введите  пароль"}))
 
     class Meta:
         model = User
         fields = ("username", "password")
 
 
-# TODO regform
-#/ firstname
-# lastname
-# uname
-# email
-# passw1
-# passw2
-# #/
+class UserRegistrationForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput
+        (attrs={'class': 'form-control py-4', "placeholder": "Введите имя пользователя"}))
+    firstname = forms.CharField(widget=forms.TextInput
+        (attrs={'class': 'form-control py-4', "placeholder": "Введите имя"}))
+    lastname = forms.CharField(widget=forms.TextInput
+        (attrs={"class": 'form-control py-4', "placeholder": "Введите фамилию"}))
+    email = forms.CharField(widget=forms.EmailInput
+        (attrs={'class': 'form-control py-4', "placegolder": "Введите адрес эл. почты"}))
+    password1 = forms.CharField(widget=forms.PasswordInput
+        (attrs={'class': 'form-control py-4', "placeholder": "Введите  пароль"}))
+    password2 = forms.CharField(widget=forms.PasswordInput
+        (attrs={'class': 'form-control py-4', "placeholder": "Введите  пароль"}))
+
+    class Meta:
+        model = User
+        fields = ("username", "firstname", "lastname", "email", "password1", "password2")
