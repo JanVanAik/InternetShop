3597c3aaa3eef95429e7309a722708e37125ba1d
JanVanAik <ar.orlovskiy@gmail.com>
2024-01-29 11:53:16 +0300

    added logout controller

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from users.views import users, register
+from users.views import users, register, logout
 
 app_name = "users"
 
 urlpatterns = [
     path('', users, name='user'),
     path('register/', register, name='register'),
+    path('logout/', logout, name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -38,3 +38,8 @@ def register(request):
         "title": "Регистрация", "form": form
     }
     return render(request, 'users/register.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse("index"))
