1b4f364a0ebe33b990d676bfd47fcf8cb2163ee9
JanVanAik <ar.orlovskiy@gmail.com>
2024-02-03 19:27:37 +0300

    worked with django.admin

--- basket/admin.py
+++ basket/admin.py
@@ -3,3 +3,9 @@ from basket.models import Basket
 
 # Register your models here.
 admin.site.register(Basket)
+
+
+class BasketAdminInline(admin.TabularInline):
+    model = Basket
+    fields = readonly_fields = ('Product', 'quantity', 'created_timestamp')
+    extra = 1
--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from users.models import User
+from basket.admin import BasketAdminInline
 # Register your models here.
 
 
-admin.site.register(User)
+
+@admin.register(User)
+class Admin(admin.ModelAdmin):
+    inlines = (BasketAdminInline,)
